659aad207f788af888a0f68d7951bf285602fd05
Prehistoric Dog <kori@tulibu-dibu.com>
2022-09-18 17:26:49 +0300

    Re-added "403" codes to output
    Re-added "403" codes to output and updated the output format.

--- scripts/invalid_site_rechecker.sh
+++ scripts/invalid_site_rechecker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for f in $(awk -F " " '/url:/ {print $2}' _data/sites.yml)
 do
-  echo $f": "$(curl -o /dev/null -sw "%{response_code}" $f) | grep -v '200\|301\|302\|403' &
+  echo $f" Response code: "$(curl -o /dev/null -sw "%{response_code}" $f) | grep -v '200\|301\|302' &
 done
