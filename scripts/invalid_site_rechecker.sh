58c005bc4c2f70f2dcbe2c44a941840835ba0c35
Prehistoric Dog <kori@tulibu-dibu.com>
2022-09-19 15:48:58 +0300

    Update invalid_site_rechecker.sh
    Fixed path to sites.yml, reversed output, and intentionally slowed down the script for more accurate results

--- scripts/invalid_site_rechecker.sh
+++ scripts/invalid_site_rechecker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for f in $(awk -F " " '/url:/ {print $2}' _data/sites.yml)
+for f in $(awk -F " " '/url:/ {print $2}' ../_data/sites.yml)
 do
-  echo $f" Response code: "$(curl -o /dev/null -sw "%{response_code}" $f) | grep -v '200\|301\|302' &
+  echo "Response code: "$(curl -o /dev/null -sw "%{response_code}" $f)" "$f| grep -v '200\|301\|302' 
 done
